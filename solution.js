1e8ad3d95cb343ccd93965250f1057469f83d15e
Cem <cemkgrbz@gmail.com>
2022-06-30 14:30:18 +0200

    added solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,53 @@
-// write your code in this file
+//1
+console.log(`Q1: ${6450/15} tickets are sold.`)
+
+
+//2
+
+console.log(`Q2: She makes ${500*52}$ every year.`)
+
+
+//3
+
+console.log(`Q3: [${17*100/30}]%`)
+
+//4
+
+console.log(`Q4: ${4.75*4}`)
+
+//5
+
+console.log(`Q5: ${5+6+7}`)
+
+//6
+
+console.log(`Q6: ${5*5}`)
+
+//7
+
+console.log(`Q7: ${0,5*3*4}`)
+
+//8
+
+console.log(`Q8: ${9*9*9}`)
+
+//9
+
+console.log(`Q9: €${22.35+(22.35*0.1)}, €${26.67+(26.67*0.15)}, €${35.92+(35.92*0.2)}`)
+
+//10
+
+console.log(`Q10: ${(8+6+5+9+8+2+1+8.5+7+4)/10} hours`)
+
+//11
+
+//console.log([75+70+85+90+100]/5)
+console.log(`Q11: Score in the sixth test: ${85*2-84}`)
+
+//12
+
+// x/8=0.78
+// x=6,24
+// y+6,24/9=0.80
+
+console.log(`Q12: James needs a minimum of ${(0.8*9-6.24)*100}% to get an 80% average.`)
